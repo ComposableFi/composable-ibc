ddd2074465579cb5de66b5b920e77c60abe000d6
David Salami <wizdave97@gmail.com>
2022-07-28 21:57:56 +0100

    remove unused errors

--- src/error.rs
+++ src/error.rs
@@ -3,14 +3,6 @@ use sp_std::prelude::*;
 #[derive(sp_std::fmt::Debug, PartialEq, Eq, derive_more::From)]
 /// Error definition for the light client
 pub enum BeefyClientError {
-    /// Failed to read a value from storage
-    StorageReadError,
-    /// Failed to write a value to storage
-    StorageWriteError,
-    /// Error decoding some value
-    DecodingError,
-    /// Invalid Mmr Update
-    InvalidMmrUpdate,
     /// Outdated commitment
     OutdatedCommitment {
         /// Latest beefy height stored
