a178e612e4caabc9c06df779a49f91bea617edd0
Seun Lanlege <seunlanlege@gmail.com>
2022-10-27 17:16:12 +0100

    re-organize trait impl

--- contracts/pallet-ibc/src/ics20/context.rs
+++ contracts/pallet-ibc/src/ics20/context.rs
@@ -18,6 +18,11 @@ where
 {
     type AccountId = T::AccountIdConversion;
 
+    fn get_port(&self) -> Result<PortId, Ics20Error> {
+        PortId::from_str(PORT_ID_STR)
+            .map_err(|e| Ics20Error::invalid_port_id(PORT_ID_STR.to_string(), e))
+    }
+
     fn get_channel_escrow_address(
         &self,
         port_id: &PortId,
@@ -31,17 +36,12 @@ where
             })
     }
 
-    fn get_port(&self) -> Result<PortId, Ics20Error> {
-        PortId::from_str(PORT_ID_STR)
-            .map_err(|e| Ics20Error::invalid_port_id(PORT_ID_STR.to_string(), e))
+    fn is_send_enabled(&self) -> bool {
+        Pallet::<T>::is_send_enabled()
     }
 
     fn is_receive_enabled(&self) -> bool {
         Pallet::<T>::is_receive_enabled()
-    }
-
-    fn is_send_enabled(&self) -> bool {
-        Pallet::<T>::is_send_enabled()
     }
 }
 
@@ -68,6 +68,31 @@ where
     u32: From<<T as frame_system::Config>::BlockNumber>,
 {
     type AccountId = T::AccountIdConversion;
+    fn send_coins(
+        &mut self,
+        from: &Self::AccountId,
+        to: &Self::AccountId,
+        amt: &ibc::applications::transfer::PrefixedCoin,
+    ) -> Result<(), Ics20Error> {
+        let amount: T::Balance = amt.amount.as_u256().low_u128().into();
+        let denom = amt.denom.to_string();
+        // Token should be registered already if sending an ibc asset
+        let asset_id =
+            T::IdentifyAssetId::to_asset_id(&denom).ok_or_else(|| Ics20Error::invalid_token())?;
+        <<T as Config>::MultiCurrency as Transfer<T::AccountId>>::transfer(
+            asset_id.into(),
+            &from.clone().into_account(),
+            &to.clone().into_account(),
+            amount,
+            false,
+        )
+        .map_err(|e| {
+            log::trace!(target: "pallet_ibc", "Failed to transfer ibc tokens: {:?}", e);
+            Ics20Error::invalid_token()
+        })?;
+        Ok(())
+    }
+
     fn mint_coins(
         &mut self,
         account: &Self::AccountId,
@@ -115,31 +140,6 @@ where
         .map_err(|e| {
             log::trace!(target: "pallet_ibc", "Failed to burn tokens: {:?}", e);
             Ics20Error::implementation_specific("Failed to burn tokens".to_string())
-        })?;
-        Ok(())
-    }
-
-    fn send_coins(
-        &mut self,
-        from: &Self::AccountId,
-        to: &Self::AccountId,
-        amt: &ibc::applications::transfer::PrefixedCoin,
-    ) -> Result<(), Ics20Error> {
-        let amount: T::Balance = amt.amount.as_u256().low_u128().into();
-        let denom = amt.denom.to_string();
-        // Token should be registered already if sending an ibc asset
-        let asset_id =
-            T::IdentifyAssetId::to_asset_id(&denom).ok_or_else(|| Ics20Error::invalid_token())?;
-        <<T as Config>::MultiCurrency as Transfer<T::AccountId>>::transfer(
-            asset_id.into(),
-            &from.clone().into_account(),
-            &to.clone().into_account(),
-            amount,
-            false,
-        )
-        .map_err(|e| {
-            log::trace!(target: "pallet_ibc", "Failed to transfer ibc tokens: {:?}", e);
-            Ics20Error::invalid_token()
         })?;
         Ok(())
     }
