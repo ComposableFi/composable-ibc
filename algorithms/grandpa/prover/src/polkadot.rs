76b9221e4266fa9e2654c22d35a9879f6856373a
Seun Lanlege <seunlanlege@gmail.com>
2022-10-28 11:18:19 +0100

    fix grandpa-prover

--- algorithms/grandpa/prover/src/polkadot.rs
+++ algorithms/grandpa/prover/src/polkadot.rs
@@ -18,7 +18,7 @@
 #![allow(missing_docs)]
 
 #[cfg(feature = "build-metadata-from-ws")]
-include!(concat!(env!("OUT_DIR"), "/runtime.rs"));
+include!(concat!(env!("OUT_DIR"), "/polkadot.rs"));
 
 #[cfg(not(feature = "build-metadata-from-ws"))]
 pub use subxt_generated::rococo::*;
