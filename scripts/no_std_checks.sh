815f7b34deec499ec223e8d6a5fc9eaa8f9520a0
Seun Lanlege <seunlanlege@gmail.com>
2022-10-31 00:40:35 +0100

    check light-client-common

--- scripts/no_std_checks.sh
+++ scripts/no_std_checks.sh
@@ -8,6 +8,7 @@ cargo +nightly check -p beefy-light-client --no-default-features --target=wasm32
 cargo +nightly check -p grandpa-light-client-primitives --no-default-features --target=wasm32-unknown-unknown
 cargo +nightly check -p grandpa-light-client-verifier --no-default-features --target=wasm32-unknown-unknown
 cargo +nightly check -p ibc --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p light-client-common --no-default-features --target=wasm32-unknown-unknown
 cargo +nightly check -p ics07-tendermint --no-default-features --target=wasm32-unknown-unknown
 cargo +nightly check -p ics10-grandpa --no-default-features --target=wasm32-unknown-unknown
 cargo +nightly check -p ics11-beefy --no-default-features --target=wasm32-unknown-unknown
