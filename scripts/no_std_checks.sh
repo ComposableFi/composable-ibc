10e0ee2da9943afc61e490079e17a71bcd873e1f
Seun Lanlege <seunlanlege@gmail.com>
2022-10-30 02:26:18 +0100

    add runtime benchmarks test

--- scripts/no_std_checks.sh
+++ scripts/no_std_checks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cargo check -p beefy-light-client-primitives --no-default-features --target=wasm32-unknown-unknown
-cargo check -p beefy-light-client --no-default-features --target=wasm32-unknown-unknown
-cargo check -p grandpa-light-client-primitives --no-default-features --target=wasm32-unknown-unknown
-cargo check -p grandpa-light-client --no-default-features --target=wasm32-unknown-unknown
-cargo check -p ibc --no-default-features --target=wasm32-unknown-unknown
-cargo check -p ics07-tendermint --no-default-features --target=wasm32-unknown-unknown
-cargo check -p ics10-grandpa --no-default-features --target=wasm32-unknown-unknown
-cargo check -p ics11-beefy --no-default-features --target=wasm32-unknown-unknown
-cargo check -p ics13-beefy --no-default-features --target=wasm32-unknown-unknown
-cargo check -p pallet-ibc --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p beefy-light-client-primitives --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p beefy-light-client --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p grandpa-light-client-primitives --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p grandpa-light-client --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p ibc --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p ics07-tendermint --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p ics10-grandpa --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p ics11-beefy --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p ics13-beefy --no-default-features --target=wasm32-unknown-unknown
+cargo +nightly check -p pallet-ibc --no-default-features --target=wasm32-unknown-unknown
