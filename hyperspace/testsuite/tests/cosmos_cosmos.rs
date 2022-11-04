64ebb67be7e0dbee742bc7758cb771f7c7894ef5
Farhad Shabani <farhad.shabani@gmail.com>
2022-11-03 19:28:08 -0700

    Check if CosmosClient instance connects
    * Do so through the health() and status() methods

--- hyperspace/testsuite/tests/cosmos_cosmos.rs
+++ hyperspace/testsuite/tests/cosmos_cosmos.rs
@@ -1,8 +1,8 @@
 use std::{marker::PhantomData, str::FromStr};
 
-use hyperspace_cosmos::{CosmosClient, CosmosClientConfig};
 use futures::StreamExt;
 use hyperspace_core::logging;
+use hyperspace_cosmos::{CosmosClient, CosmosClientConfig};
 use hyperspace_primitives::{mock::LocalClientTypes, IbcProvider, KeyProvider};
 use hyperspace_testsuite::{
 	ibc_channel_close, ibc_messaging_packet_height_timeout_with_connection_delay,
@@ -18,7 +18,7 @@ use tendermint_rpc::{endpoint::broadcast::tx_sync::Response, Client, HttpClient,
 
 async fn setup_clients<H: Clone + Send + Sync + 'static>() -> (CosmosClient<H>, CosmosClient<H>) {
 	log::info!(target: "hyperspace", "=========================== Starting Test ===========================");
-	
+
 	// Create client configurations
 	// Parameters have been set up to work with local nodes according to https://hermes.informal.systems/tutorials
 	let config_a = CosmosClientConfig {
@@ -49,6 +49,15 @@ async fn setup_clients<H: Clone + Send + Sync + 'static>() -> (CosmosClient<H>, 
 
 	let mut chain_a = CosmosClient::<H>::new(config_a).await.unwrap();
 	let mut chain_b = CosmosClient::<H>::new(config_b).await.unwrap();
+
+	// Wait until for cosmos chains to start producing blocks
+	log::info!(target: "hyperspace", "Waiting for block production from cosmos chains");
+	chain_a.rpc_client.health().await.unwrap();
+	chain_a.rpc_client.status().await.unwrap();
+	chain_b.rpc_client.health().await.unwrap();
+	chain_b.rpc_client.status().await.unwrap();
+	log::info!(target: "hyperspace", "Cosmos chains are ready");
+
 	(chain_a, chain_b)
 }
 
