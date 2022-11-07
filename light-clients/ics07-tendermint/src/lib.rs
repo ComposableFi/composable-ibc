2468fd1b3d91ae3a03f99474c7a447b140432f06
Seun Lanlege <seunlanlege@gmail.com>
2022-11-07 12:57:45 +0100

    cargo fmt

--- light-clients/ics07-tendermint/src/lib.rs
+++ light-clients/ics07-tendermint/src/lib.rs
@@ -12,7 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #![allow(clippy::all)]
 
 //! ICS 07: Tendermint Client implements a client verification algorithm for blockchains which use
