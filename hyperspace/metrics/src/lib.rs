282906cbd2ddca769e997eb4b05aed324498c156
Web3 Philosopher <seun@composable.finance>
2022-11-07 18:20:14 +0100

    Add apache license header to all files (#105)
    * add apache license header to all files
    
    * cargo fmt

--- hyperspace/metrics/src/lib.rs
+++ hyperspace/metrics/src/lib.rs
@@ -1,7 +1,4 @@
-// This file is part of Substrate.
-
-// Copyright (C) 2019-2022 Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 ComposableFi
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
