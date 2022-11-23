4de198fc5dfbb57a779b6004d092f4e96d416753
Farhad Shabani <farhad.shabani@gmail.com>
2022-11-23 12:05:23 -0800

    Listen for block production by Cosmos chains

--- hyperspace/testsuite/tests/cosmos_cosmos.rs
+++ hyperspace/testsuite/tests/cosmos_cosmos.rs
@@ -1,5 +1,5 @@
 use hyperspace_core::logging;
-use hyperspace_cosmos::{key_provider::KeyEntry, CosmosClient, CosmosClientConfig};
+use hyperspace_cosmos::{key_provider::KeyEntry, IbcProvider, CosmosClient, CosmosClientConfig};
 use hyperspace_testsuite::{
 	ibc_channel_close, ibc_messaging_packet_height_timeout_with_connection_delay,
 	ibc_messaging_packet_timeout_on_channel_close,
@@ -8,6 +8,20 @@ use hyperspace_testsuite::{
 };
 use std::{path::PathBuf, str::FromStr};
 use tendermint_rpc::Url;
+use crate::cosmos::{client::CosmosClient, client::CosmosClientConfig, key_provider::KeyEntry};
+use futures::{future, StreamExt};
+use ibc::events::IbcEvent;
+use std::{path::PathBuf, str::FromStr};
+use tendermint_rpc::Url;
+use ibc::{
+    applications::transfer::VERSION,
+    core::{
+        ics04_channel::channel::{ChannelEnd, Order, State},
+        ics24_host::identifier::{ChannelId, ConnectionId, PortId},
+    },
+};
+use std::time::Duration;
+use tokio::task::JoinHandle;
 
 async fn setup_clients<H: Clone + Send + Sync + 'static>() -> (CosmosClient<H>, CosmosClient<H>) {
 	log::info!(target: "hyperspace", "=========================== Starting Test ===========================");
@@ -48,13 +62,32 @@ async fn setup_clients<H: Clone + Send + Sync + 'static>() -> (CosmosClient<H>, 
 
 	let mut chain_a = CosmosClient::<H>::new(config_a).await.unwrap();
 	let mut chain_b = CosmosClient::<H>::new(config_b).await.unwrap();
+
+	// Wait until for cosmos to start producing blocks
+	log::info!(target: "hyperspace", "Waiting for block production from Cosmos chains");
+	let _ = chain_a
+		.ibc_events()
+		.await
+		.skip_while(|ev| future::ready(!matches!(ev, IbcEvent::NewBlock(_))))
+		.take(1)
+		.collect::<Vec<_>>()
+		.await;
+	let _ = chain_b
+		.ibc_events()
+		.await
+		.skip_while(|ev| future::ready(!matches!(ev, IbcEvent::NewBlock(_))))
+		.take(1)
+		.collect::<Vec<_>>()
+		.await;
+	log::info!(target: "hyperspace", "Cosmos chains are ready to go!");
+	 
 	todo!()
 }
 
 #[tokio::test]
 async fn cosmos_to_cosmos_ibc_messaging_full_integration_test() {
-	// logging::setup_logging();
-	// let (mut chain_a, mut chain_b) = setup_clients::<u32>().await;
+	logging::setup_logging();
+	let (mut chain_a, mut chain_b) = setup_clients::<()>().await;
 	// Run tests sequentially
 
 	// no timeouts + connection delay
