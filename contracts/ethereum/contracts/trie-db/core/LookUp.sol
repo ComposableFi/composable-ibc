dac9d1ed28daebad4dd57bf42d02cad9a11758f0
nadeemb53 <nadeemb53@gmail.com>
2022-11-30 00:31:08 +0530

    fix trie interface import

--- contracts/ethereum/contracts/trie-db/core/LookUp.sol
+++ contracts/ethereum/contracts/trie-db/core/LookUp.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "./Trie.sol";
+import "../interfaces/ITrie.sol";
 import "./Codec.sol";
 import "./Node.sol";
 import "./NibbleSlice.sol";
