49767338a5b21395272321d2e8eb88803c470a91
nadeemb53 <nadeemb53@gmail.com>
2022-12-10 12:45:50 +0530

    init node header

---
+++ contracts/ethereum/contracts/node-codec/interfaces/ICodec.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+interface ICodec {
+    enum NodeHeaderType {
+        Null,
+        Branch,
+        Leaf,
+        HashedValueBranch,
+        HashedValueLeaf
+    }
+
+    struct NodeHeaderStruct {
+        NodeHeaderType headerType;
+        bool isValue;
+        uint256 value;
+    }
+}
