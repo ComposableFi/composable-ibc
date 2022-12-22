b2e561f7dc18a99de02e33051c05af049a5f1bb7
nadeemb53 <nadeemb53@gmail.com>
2022-12-22 14:39:31 +0530

    foundry x hardhat directory structure

--- contracts/ethereum/src/core/node-codec/ScaleCodec.sol
+++ contracts/ethereum/src/core/node-codec/ScaleCodec.sol
@@ -4,7 +4,5 @@ pragma solidity ^0.8.17;
 import "../../interfaces/ICodec.sol";
 
 contract ScaleCodec is ICodec {
-    function decode(uint8[] memory data) external returns (uint8) {}
-
-    function _readByte() internal {}
+    function decode(uint8[] memory input) external pure returns (uint8) {}
 }
