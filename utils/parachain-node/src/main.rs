aa863099cdd350f51eb1d8cb92c2c5c1c6799850
Web3 Smith <31099392+Wizdave97@users.noreply.github.com>
2023-01-02 14:06:17 +0100

    Client expiry test (#169)
    * simnode setup
    
    * simnode test failing
    
    * assert client expiry
    
    * fix
    
    * fix tests
    
    * fix ibc tests
    
    * move simnode tests to a new task
    
    * skip wasm build
    
    * adding wasm target to CI toolchain

--- utils/parachain-node/src/main.rs
+++ utils/parachain-node/src/main.rs
@@ -16,12 +16,7 @@
 
 #![warn(missing_docs)]
 
-mod chain_spec;
-#[macro_use]
-mod service;
-mod cli;
-mod command;
-mod rpc;
+use parachain_node::command;
 
 fn main() -> sc_cli::Result<()> {
 	command::run()
