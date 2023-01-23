9a3b40146b9c7cfbb0b294034d4aa4bdf009c8f5
nadeemb53 <nadeemb53@gmail.com>
2023-01-23 08:47:46 +0530

    fix: only clone

--- contracts/pallet-ibc/rpc/src/lib.rs
+++ contracts/pallet-ibc/rpc/src/lib.rs
@@ -97,7 +97,7 @@ impl<Hash: std::fmt::Debug> Display for BlockNumberOrHash<Hash> {
 }
 
 /// Proof for a set of keys
-#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Proof {
 	/// Trie proof
 	pub proof: Vec<u8>,
