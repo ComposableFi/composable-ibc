b70af4e84dc0f63ab9b9a25eeeaa6aa7060de318
David Salami <wizdave97@gmail.com>
2023-01-25 09:29:58 +0100

    remove chunking in grandpa subscription

--- hyperspace/parachain/src/chain.rs
+++ hyperspace/parachain/src/chain.rs
@@ -151,8 +151,7 @@ where
 						&*self.relay_ws_client,
 					)
 						.await
-						.expect("Failed to subscribe to grandpa justifications")
-						.chunks(6);
+						.expect("Failed to subscribe to grandpa justifications");
 
 				let stream = subscription.filter_map(|justification_notif| {
 					let encoded_justification = match justification_notif {
