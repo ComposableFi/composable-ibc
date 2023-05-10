370be9e6fedfd9f4899bbe17becb3a02bba41b65
cr4pt0 <placex.com@gmail.com>
2023-05-10 16:53:39 +0100

    fix cargo fmt

--- contracts/pallet-ibc/src/tests.rs
+++ contracts/pallet-ibc/src/tests.rs
@@ -3,8 +3,8 @@ use crate::{
 	light_clients::{AnyClientState, AnyConsensusState},
 	mock::*,
 	routing::Context,
-	Any, Config, ConsensusHeights, DenomToAssetId, MultiAddress, Pallet,
-	PendingRecvPacketSeqs, PendingSendPacketSeqs, Timeout, TransferParams, MODULE_ID,
+	Any, Config, ConsensusHeights, DenomToAssetId, MultiAddress, Pallet, PendingRecvPacketSeqs,
+	PendingSendPacketSeqs, Timeout, TransferParams, MODULE_ID,
 };
 use core::time::Duration;
 use frame_support::{
