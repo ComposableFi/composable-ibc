b54840893f5e5250be1c083c8f03c432341b642b
Rust Ninja <102041955+RustNinja@users.noreply.github.com>
2023-05-11 22:57:17 +0100

    Charge IBC fee on sender chain if channels are not whitelisted (#310)
    * charge IBC fee on sender chain with whitelists channels.
    
    WIP some logic on transfer extrinsic
    
    * implement charging fee in transfer extrinsic of pallet-ibc
    
    * add FailedSendFeeToAccount error
    
    * add OriginAddress error
    
    * add logic to on_acknowledgement_packet callback
    
    * remove 				// <Pallet<T> as Store>::SequenceFee::try_mutate_exists
    
    * Add Refund on timeout packet callback and add extrinsic to add/remove channels from whitelist (#311)
    
    * update on_acknowledgement_packet
    
    * finished on_timeout_packet
    
    * add two extrinsic to add/remove channels from free fee whitelist
    
    * reconfigure mock runtime and fix unit tests.
    
    * check fee in send_transfer test function.
    
    * add unit tests for whitelisted channels to not charge the fee.
    
    * add event when charge fee on transfer extrinsic
    
    * add is_flat_fee data to event on transfer extrinsic
    
    * add event when fee refunded to sender account
    
    * Add free fee whitelist when receive coin via IBC on deliver (#312)
    
    introduce free fee whitelist storage for channels  when receive coins
    
    * add docs for refund_fee function.
    
    * rename ServiceCharge to ServiceChargeIn and ServiceChargeOut
    
    * fix tests and mock
    
    * add docs for ServiceChargeOut and ServiceChargeIn
    
    * fix sp_runtime Perbill import
    
    * replace <Pallet<T> as Store>::SequenceFee: to SequenceFee::<T>::
    
    * replace singer_from to signer_from
    
    * update pr according to request changes.
    
    * update pr according to request changes
    
    * update parachain-runtime to use new associated type for ibc config
    
    * remove unused imports
    
    * fix cargo fmt
    
    * update IbcIcs20ServiceCharge to 0 percentage
    
    ---------
    
    Co-authored-by: Blas Rodriguez Irizar <rodrigblas@gmail.com>

--- contracts/pallet-ibc/src/mock.rs
+++ contracts/pallet-ibc/src/mock.rs
@@ -219,6 +219,10 @@ impl Config for Test {
 	type Ics20RateLimiter = Everything;
 	type FeeAccount = FeeAccount;
 	type CleanUpPacketsPeriod = CleanUpPacketsPeriod;
+	type ServiceChargeOut = ServiceCharge;
+	type FlatFeeConverter = FlatFeeConverterDummy<Test>;
+	type FlatFeeAssetId = FlatFeeAssetId;
+	type FlatFeeAmount = FlatFeeAmount;
 }
 #[derive(Debug, Clone)]
 pub struct FlatFeeConverterDummy<T: Config>(PhantomData<T>);
@@ -238,11 +242,8 @@ impl<T: Config> FlatFeeConverter for FlatFeeConverterDummy<T> {
 }
 impl crate::ics20_fee::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
-	type ServiceCharge = ServiceCharge;
+	type ServiceChargeIn = ServiceCharge;
 	type PalletId = PalletId;
-	type FlatFeeConverter = FlatFeeConverterDummy<Test>;
-	type FlatFeeAssetId = FlatFeeAssetId;
-	type FlatFeeAmount = FlatFeeAmount;
 }
 
 #[derive(
