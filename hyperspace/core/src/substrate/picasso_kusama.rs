78d86f8d2e023de9d35f6979c36617a69f79e731
Vladislav Markushin <negigic@gmail.com>
2023-05-12 12:57:16 -0300

    Add MetadataIbcEvent::PushWasmCode for kusama

--- hyperspace/core/src/substrate/picasso_kusama.rs
+++ hyperspace/core/src/substrate/picasso_kusama.rs
@@ -134,7 +134,11 @@ define_runtime_transactions!(
 	|| unimplemented("ibc_increase_counters is not implemented")
 );
 
-define_ibc_event_wrapper!(IbcEventWrapper, MetadataIbcEvent,);
+define_ibc_event_wrapper!(
+	IbcEventWrapper,
+	MetadataIbcEvent,
+	MetadataIbcEvent::PushWasmCode { wasm_code_id } => RawIbcEvent::PushWasmCode { wasm_code_id },
+);
 
 define_event_record!(
 	PicassoEventRecord,
