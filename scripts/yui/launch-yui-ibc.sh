678f6c40fef8a5f663cefa9a0669c3339419bad2
Chris Ivanov <chris@composable.finance>
2023-05-12 13:14:35 +0100

    fix: launch-yui-ibc wrt how the OwnableIBCHandler is created

--- scripts/yui/launch-yui-ibc.sh
+++ scripts/yui/launch-yui-ibc.sh
@@ -21,6 +21,12 @@ yarn --version
 node --version
 jq --version
 
+
+if [[ lsof -t -i :8545 ]]; then
+    echo "port 8545 is already in use"
+    exit 1
+fi
+
 # if the /tmp/yui-ibc-solidity directory does not exist, clone the repo
 if [ ! -d "/tmp/yui-ibc-solidity" ]; then
     git clone https://github.com/hyperledger-labs/yui-ibc-solidity /tmp/yui-ibc-solidity
@@ -30,34 +36,34 @@ cd /tmp/yui-ibc-solidity
 
 yarn
 
-if [[ lsof -t -i :8545 ]]; then
-    echo "port 8545 is already in use"
-    exit 1
-fi
-
+# start local ethereum node using anvil
 anvil --mnemonic "$MNEMONIC" --host 0.0.0.0 --port 8545 &
 netstat -an | grep 8545
 
+# deploy the IBC contracts
 forge create -r http://localhost:8545 --mnemonic "$MNEMONIC" IBCClient --json > ibc-client.json
-export IBC_CLIENT=`cat ibc-client.json | jq '.deployedTo'`
 
 forge create -r http://localhost:8545 --mnemonic "$MNEMONIC" IBCConnection --json > ibc-connection.json
 cat ibc-connection.json
-export IBC_CONNECTION=`cat ibc-connection.json | jq '.deployedTo'`
 
 forge create -r http://localhost:8545 --mnemonic "$MNEMONIC" IBCChannelHandshake --json > ibc-channel-handshake.json
 cat ibc-channel-handshake.json
-export IBC_CHANNEL_HANDSHAKE=`cat ibc-channel-handshake.json | jq '.deployedTo'`
 
 forge create -r http://localhost:8545 --mnemonic "$MNEMONIC" IBCPacket --json > ibc-packet.json
 cat ibc-packet.json
-export IBC_PACKET=`cat ibc-packet.json | jq '.deployedTo'`
 
-forge create -r http://localhost:8545 --mnemonic=$MNEMONIC OwnableIBCHandler --constructor-args $IBC_CLIENT $IBC_CONNECTION $IBC_CHANNEL_HANDSHAKE $IBC_PACKET --json > ownable-ibc-handler.json
+forge create -r http://localhost:8545 --mnemonic "$MNEMONIC" OwnableIBCHandler \
+    --constructor-args\
+        `cat ibc-client.json | jq '.deployedTo' | tr -d '"'` \
+        `cat ibc-connection.json | jq '.deployedTo' | tr -d '"'` \
+        `cat ibc-channel-handshake.json | jq '.deployedTo' | tr -d '"'` \
+        `cat ibc-packet.json | jq '.deployedTo' | tr -d '"'` \
+    --json > ownable-ibc-handler.json
+
 cat ownable-ibc-handler.json
-export OWNABLE_IBC_HANDLER=`cat ownable-ibc-handler.json | jq '.deployedTo'`
 
 cast rpc --rpc-url http://127.0.0.1:8545 eth_accounts
 
+# setup a trap to kill the anvil process when the script exits
 trap "kill -9 `jobs -p`" SIGHUP
 fg anvil
