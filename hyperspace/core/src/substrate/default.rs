5052a8dae8f47d5088689d13e4876d497ad65926
Rust Dev <102041955+RustNinja@users.noreply.github.com>
2023-05-15 16:20:58 +0100

    Fix build that broken by PushWasmCode (#319)
    * fix build that broken by PushWasmCode

--- hyperspace/core/src/substrate/default.rs
+++ hyperspace/core/src/substrate/default.rs
@@ -131,11 +131,7 @@ define_runtime_transactions!(
 	)
 );
 
-define_ibc_event_wrapper!(
-	IbcEventWrapper,
-	MetadataIbcEvent,
-	MetadataIbcEvent::PushWasmCode { wasm_code_id } => RawIbcEvent::PushWasmCode { wasm_code_id },
-);
+define_ibc_event_wrapper!(IbcEventWrapper, MetadataIbcEvent,);
 
 define_event_record!(
 	DefaultEventRecord,
