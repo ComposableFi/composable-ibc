5828ff1e3450a63f8258dd7411964e9099ded4e6
Vladislav Markushin <negigic@gmail.com>
2023-05-15 13:55:44 -0300

    Update ics23 crate

--- contracts/pallet-ibc/simple-iavl/src/avl/mod.rs
+++ contracts/pallet-ibc/simple-iavl/src/avl/mod.rs
@@ -4,7 +4,7 @@
 //! `insert` instructions (no delete yet, it's not needed as the on-chain store is supposed to be
 //! immutable).
 //!
-//! Proof of existence are supported using [ICS23](https://github.com/confio/ics23), but proof of
+//! Proof of existence are supported using [ICS23](https://github.com/cosmos/ics23), but proof of
 //! non-existence are not yet implemented.
 //!
 //! Keys needs to implement `Ord` and `AsBytes` (see `as_bytes` module), while values are required
@@ -15,16 +15,16 @@
 use tendermint::hash::Algorithm;
 
 pub use as_bytes::{AsBytes, ByteSlice};
+pub use ics23::tendermint_spec;
 pub use node::AvlNode;
-pub use proof::get_proof_spec;
 pub use tree::AvlTree;
 
 mod as_bytes;
 mod node;
-mod proof;
 mod tree;
 
 #[cfg(test)]
 mod tests;
 
 const HASH_ALGO: Algorithm = Algorithm::Sha256;
+pub const LEAF_PREFIX: [u8; 64] = [0; 64]; // 64 bytes of zeroes.
