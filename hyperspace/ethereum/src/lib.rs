e669d0792b206d44a27d8a09c0e798c58bb7cb94
Chris <chris@composable.finance>
2023-05-16 12:43:37 +0100

    feat: Add contract module
    This commit adds a new `contract` module to the Ethereum library, which includes a function for creating a new contract instance from an address and ABI. The ABI is included as a constant in JSON format.

--- hyperspace/ethereum/src/lib.rs
+++ hyperspace/ethereum/src/lib.rs
@@ -4,6 +4,28 @@ pub mod config;
 
 pub mod client;
 
+pub mod contract {
+	use std::sync::Arc;
+
+	use ethers::{
+		abi::{Abi, Address},
+		prelude::Contract,
+		providers::Middleware,
+	};
+
+	pub const ABI_JSON: &str = include_str!("./contract-abi.json");
+
+	/// Create a new contract instance from the given address and ABI.
+	#[track_caller]
+	pub fn contract<M>(address: Address, client: Arc<M>) -> Contract<M>
+	where
+		M: Middleware,
+	{
+		let abi: Abi = serde_json::from_str(ABI_JSON).unwrap();
+		Contract::new(address, abi, client)
+	}
+}
+
 pub mod chain;
 pub mod ibc_provider;
 pub mod key_provider;
