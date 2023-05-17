19abaad0776ae1c64ee8091609b4a451bb9e5bd0
Chris <chris@composable.finance>
2023-05-17 14:11:29 +0100

    Add Yui storage script to clone and install dependencies
    This commit adds a new bash script that clones the yui-ibc-solidity repository if it doesn't exist, checks out version v0.3.5, installs yarn and forge dependencies, and inspects the OwnableIBCHandler storage layout.

---
+++ scripts/yui/yui-storage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxm
+
+export ROOT=`git rev-parse --show-toplevel`
+
+# if the /tmp/yui-ibc-solidity directory does not exist, clone the repo
+if [ ! -d "/tmp/yui-ibc-solidity" ]; then
+    git clone -q https://github.com/hyperledger-labs/yui-ibc-solidity /tmp/yui-ibc-solidity
+fi
+
+cd /tmp/yui-ibc-solidity
+git checkout bfd4c30 # v0.3.5
+
+yarn > /dev/null 2>&1
+forge install > /dev/null 2>&1
+forge inspect OwnableIBCHandler storage_layout
