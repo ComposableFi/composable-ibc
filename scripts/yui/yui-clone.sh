717578fb0f7b6fff0e9626773e0d954becc01f67
Chris <chris@composable.finance>
2023-06-01 14:11:02 +0100

    Refactor Makefile and yui-clone.sh, delete yui-light-client-abi.sh, and modify yui-storage.sh.
    The Makefile in hyperspace was refactored to remove a script that generated contract storage layout. The yui-clone.sh script was modified to use environment variables for the IBC path and repository. The script also checks if the directory exists before cloning the repo. The yui-light-client-abi.sh script was deleted as it is no longer needed. Finally, the forge inspect command in yui-storage.sh now takes an argument for which contract's storage layout to inspect instead of always using OwnableIBCHandler.

--- scripts/yui/yui-clone.sh
+++ scripts/yui/yui-clone.sh
@@ -3,22 +3,24 @@
 function yui_clone() {
     # use the already set $YUI_COMMIT or set it to bfd4c30 (v0.3.5) if not set
     export YUI_COMMIT=${YUI_COMMIT:-bfd4c30}
-    
+    # use the already set $YUI_IBC_PATH or set it to /tmp/yui-ibc-solidity if not set
+    export YUI_IBC_PATH=${YUI_IBC_PATH:-/tmp/yui-ibc-solidity} 
+    # use the already set $YUI_IBC_REPO or set it to the yui-ibc-solidity-private repo if not set
+    export YUI_IBC_REPO=${YUI_IBC_REPO:-https://github.com/ComposableFi/yui-ibc-solidity-private}
+
+
     # if the /tmp/yui-ibc-solidity directory does not exist, clone the repo
-    if [ ! -d "/tmp/yui-ibc-solidity" ]; then
-        git clone https://github.com/hyperledger-labs/yui-ibc-solidity /tmp/yui-ibc-solidity
+    if [ ! -d $YUI_IBC_PATH ]; then
+        git clone $YUI_IBC_REPO $YUI_IBC_PATH
 
-        cd /tmp/yui-ibc-solidity
+        cd $YUI_IBC_PATH
 
-        git checkout $YUI_COMMIT
-        
-        # apply patches from ../scripts/yui/patches/*.patch
-        for patch in $(ls $ROOT/scripts/yui/patches/*.patch); do
-            git apply $patch
-        done
+        if [ ! -z $YUI_COMMIT ]; then
+            git checkout $YUI_COMMIT
+        fi
 
         yarn
     else
-        cd /tmp/yui-ibc-solidity
+        cd $YUI_IBC_PATH
     fi
 }
