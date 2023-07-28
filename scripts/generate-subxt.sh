19756a8e7bdc8ba7d7a5c0183ea7c871fe69ace7
Vladislav <negigic@gmail.com>
2023-07-28 13:53:31 -0300

    Accept full uri in codegen instead of host and port, update docs (#379)

--- scripts/generate-subxt.sh
+++ scripts/generate-subxt.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-cargo +nightly run --bin codegen -- --path ./utils/subxt/generated/src/default
-cargo +nightly fmt -- --emit=files
-if [[ -z $(git status -s) ]]
-then
-  echo "Generated subxt types are up to date"
-else
-  echo "Subxt types are outdated, please generate subxt types for the new runtime."
-#  exit 1
-fi
+cargo run --bin codegen -- --path ./utils/subxt/generated/src/default
+cargo fmt -- --emit=files
+
+echo "Subxt types are up to date"
--- utils/subxt/codegen/bin/main.rs
+++ utils/subxt/codegen/bin/main.rs
@@ -12,9 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use anyhow::anyhow;
 use clap::Parser;
 use codec::Encode;
 use codegen::{codegen, fetch_metadata_ws};
+use http::uri::{Authority, Uri};
 use std::{path::PathBuf, str::FromStr};
 use wasm_testbed::WasmTestBed;
 
@@ -22,20 +24,41 @@ use wasm_testbed::WasmTestBed;
 pub struct Cli {
 	#[clap(long)]
 	pub path: PathBuf,
-	#[clap(long, env = "RELAY_HOST", default_value = "ws://127.0.0.1")]
-	pub relay_host: String,
-	#[clap(long, default_value = "9944")]
-	pub relay_port: String,
-	#[clap(long, env = "PARA_HOST", default_value = "ws://127.0.0.1")]
-	pub para_host: String,
-	#[clap(long, default_value = "9988")]
-	pub para_port: String,
+	#[clap(long, env = "RELAY_HOST", default_value = "ws://127.0.0.1:9944")]
+	pub relay_url: Uri,
+	#[clap(long, env = "PARA_HOST", default_value = "ws://127.0.0.1:9988")]
+	pub para_url: Uri,
 	/// Path to parachain runtime wasm blob
 	#[clap(long)]
 	pub parachain_wasm: Option<PathBuf>,
 	/// Path to relaychain runtime wasm
 	#[clap(long)]
 	pub relaychain_wasm: Option<PathBuf>,
+}
+
+fn format_uri(uri: Uri) -> anyhow::Result<Uri> {
+	let default_port = |scheme| match scheme {
+		"http" | "ws" => Some(80u16),
+		"https" | "wss" => Some(443),
+		_ => None,
+	};
+
+	let scheme = uri.scheme_str().unwrap_or(&"wss");
+	let authority = uri.authority().ok_or_else(|| anyhow!("No authority in {uri}"))?;
+	let port = match authority.port_u16() {
+		None => default_port(scheme)
+			.ok_or_else(|| anyhow!("No default port for scheme {} in {uri}", scheme))?,
+		Some(p) => p,
+	};
+	let mut builder = Uri::builder().scheme(scheme).authority(Authority::from_str(&format!(
+		"{}:{}",
+		authority.host(),
+		port
+	))?);
+	if let Some(path) = uri.path_and_query().cloned() {
+		builder = builder.path_and_query(path);
+	}
+	Ok(builder.build()?)
 }
 
 #[tokio::main]
@@ -53,19 +76,16 @@ async fn main() -> Result<(), anyhow::Error> {
 			tokio::fs::write(&path, &code).await?;
 		}
 	} else {
-		if !cli.relay_host.contains("://") {
-			cli.relay_host = format!("ws://{}", cli.relay_host);
-		}
-		if !cli.para_host.contains("://") {
-			cli.para_host = format!("ws://{}", cli.para_host);
-		}
-		let runtimes = [
-			(format!("{}:{}", cli.relay_host, cli.relay_port), "relaychain"),
-			(format!("{}:{}", cli.para_host, cli.para_port), "parachain"),
-		];
+		cli.relay_url = format_uri(cli.relay_url)?;
+		cli.para_url = format_uri(cli.para_url)?;
+
+		let runtimes = [(cli.relay_url, "relaychain"), (cli.para_url, "parachain")];
 
 		for (url, runtime) in runtimes {
-			let metadata = fetch_metadata_ws(&url).await?;
+			let url_str = url.to_string();
+			let metadata = fetch_metadata_ws(&url_str)
+				.await
+				.map_err(|e| anyhow!("Failed to fetch metadata from {}: {}", url_str, e))?;
 			let code = codegen(&mut &metadata[..])?;
 			let path = cli.path.join(format!("{runtime}.rs"));
 			tokio::fs::write(&path, &code).await?;
