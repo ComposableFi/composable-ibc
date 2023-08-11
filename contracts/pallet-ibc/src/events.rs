4f249f4ca5734aa43d5fcfc6bfbc644dd3da8f83
Vladislav Markushin <negigic@gmail.com>
2023-08-11 13:05:50 -0300

    Add 'acks_str' field to IbcEvent struct

--- contracts/pallet-ibc/src/events.rs
+++ contracts/pallet-ibc/src/events.rs
@@ -194,6 +194,7 @@ pub enum IbcEvent {
 		dest_port: Vec<u8>,
 		dest_channel: Vec<u8>,
 		sequence: u64,
+		acks_str: String,
 	},
 	/// Timeout packet
 	TimeoutPacket {
@@ -410,6 +411,7 @@ impl From<RawIbcEvent> for IbcEvent {
 				dest_port: ev.dst_port_id().as_bytes().to_vec(),
 				dest_channel: ev.dst_channel_id().to_string().as_bytes().to_vec(),
 				sequence: ev.packet.sequence.into(),
+				acks_str: String::from_utf8_lossy(&ev.ack).to_string(),
 			},
 			RawIbcEvent::AcknowledgePacket(ev) => IbcEvent::AcknowledgePacket {
 				revision_height: ev.height().revision_height,
@@ -964,6 +966,7 @@ impl TryFrom<IbcEvent> for RawIbcEvent {
 				dest_port,
 				dest_channel,
 				sequence,
+				acks_str: _,
 			} => Ok(RawIbcEvent::WriteAcknowledgement(ChannelEvents::WriteAcknowledgement {
 				height: Height::new(revision_number, revision_height),
 				packet: Packet {
