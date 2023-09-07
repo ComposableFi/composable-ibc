c0285a968788f65209d9f530d7ee08a8f9c0d72a
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-09-07 10:22:39 +0100

    reduced amount of blocks to wait, tested locally - ports are super ok and make READY status

--- scripts/zombienet/wait-for-parachains-network.sh
+++ scripts/zombienet/wait-for-parachains-network.sh
@@ -3,7 +3,7 @@
 set -e
 block_number=0
 
-until [ $block_number -gt 70 ]
+until [ $block_number -gt 10 ]
 do
   block_number=$(curl -L -s http://${PARA_HOST}:$METRICS_PORT/metrics | grep 'substrate_block_height{status="best"' | awk '{print $2}')
   if [ -z "$block_number" ]
