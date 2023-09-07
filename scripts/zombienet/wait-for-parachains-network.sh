58e7772ec32819156e540160a2000068824e8bcb
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2023-09-06 23:42:44 -0300

    query PARA_HOST

--- scripts/zombienet/wait-for-parachains-network.sh
+++ scripts/zombienet/wait-for-parachains-network.sh
@@ -5,7 +5,7 @@ block_number=0
 
 until [ $block_number -gt 70 ]
 do
-  block_number=$(curl -L -s http://127.0.0.1:$METRICS_PORT/metrics | grep 'substrate_block_height{status="best"' | awk '{print $2}')
+  block_number=$(curl -L -s http://${PARA_HOST}:$METRICS_PORT/metrics | grep 'substrate_block_height{status="best"' | awk '{print $2}')
   if [ -z "$block_number" ]
   then
     cat /tmp/logifle.log
