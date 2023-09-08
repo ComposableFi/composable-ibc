4e010e5f2eaa65f857f1cb7973a2a5a1c51e2b17
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2023-09-08 13:07:41 -0300

    feature flag BenchmarkHelper and propagate runtime-benchmarks

--- contracts/pallet-ibc/src/mock.rs
+++ contracts/pallet-ibc/src/mock.rs
@@ -173,6 +173,7 @@ impl pallet_assets::Config for Test {
 	type AssetIdParameter = Self::AssetId;
 
 	type CallbackHandle = ();
+	#[cfg(feature = "runtime-benchmarks")]
 	type BenchmarkHelper = ();
 }
 
