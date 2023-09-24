1e930f76c4a9bc8154a0a8dc23c01139af8a21e7
cr4pt0 <placex.com@gmail.com>
2023-09-24 17:37:30 +0100

    tests passed without ibc_channel_close and ibc_messaging_packet_timeout_on_channel_close

--- hyperspace/testsuite/tests/parachain_parachain.rs
+++ hyperspace/testsuite/tests/parachain_parachain.rs
@@ -186,18 +186,18 @@ async fn parachain_to_parachain_ibc_messaging_full_integration_test() {
 		res.unwrap();
 	}
 
-	return;
+	// return;
 
 	// channel closing semantics
 	let mut join_set = tokio::task::JoinSet::new();
 	let mut c1 = chain_a.clone();
 	let mut c2 = chain_b.clone();
 	// join_set.spawn(async move {
-		ibc_messaging_packet_timeout_on_channel_close(&mut c1, &mut c2, asset_id, channel_a).await;
+		// ibc_messaging_packet_timeout_on_channel_close(&mut c1, &mut c2, asset_id, channel_a).await;
 	// 	log::info!(target: "hyperspace", "🚀🚀 finished packet timeout on channel close");
 	// });
 	join_set.spawn(async move {
-		ibc_channel_close(&mut chain_aa, &mut chain_bb).await;
+		// ibc_channel_close(&mut chain_aa, &mut chain_bb).await;
 		log::info!(target: "hyperspace", "🚀🚀 finished channel close");
 	});
 
@@ -206,10 +206,10 @@ async fn parachain_to_parachain_ibc_messaging_full_integration_test() {
 		res.unwrap();
 	}
 
-	// // Test sync abilities, run this before misbehaviour test
-	// client_synchronization_test(&mut chain_a, &mut chain_b).await;
+	// Test sync abilities, run this before misbehaviour test
+	client_synchronization_test(&mut chain_a, &mut chain_b).await;
 
-	// // misbehaviour
-	// ibc_messaging_submit_misbehaviour(&mut chain_a, &mut chain_b).await;
-	// log::info!(target: "hyperspace", "🚀🚀 Waiting for misbehaviour to be submitted");
+	// misbehaviour
+	ibc_messaging_submit_misbehaviour(&mut chain_a, &mut chain_b).await;
+	log::info!(target: "hyperspace", "🚀🚀 Waiting for misbehaviour to be submitted");
 }
