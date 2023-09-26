bceb2a7fc01490c4f55aaf2ab412edbe477f2500
cr4pt0 <placex.com@gmail.com>
2023-09-26 22:55:00 +0100

    comment ibc_messaging_submit_misbehaviour. test that all other works.

--- hyperspace/testsuite/tests/parachain_parachain.rs
+++ hyperspace/testsuite/tests/parachain_parachain.rs
@@ -203,6 +203,6 @@ async fn parachain_to_parachain_ibc_messaging_full_integration_test() {
 	client_synchronization_test(&mut chain_a, &mut chain_b).await;
 
 	// misbehaviour
-	ibc_messaging_submit_misbehaviour(&mut chain_a, &mut chain_b).await;
+	// ibc_messaging_submit_misbehaviour(&mut chain_a, &mut chain_b).await;
 	log::info!(target: "hyperspace", "🚀🚀 Waiting for misbehaviour to be submitted");
 }
