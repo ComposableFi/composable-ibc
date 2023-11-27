3009063341f088e5c41ed1a10d4c096422f0ee8a
Carlos Rodriguez <carlos@interchain.io>
2023-11-27 14:55:53 +0100

    add missing annotations

--- light-clients/ics08-wasm/src/instantiate.rs
+++ light-clients/ics08-wasm/src/instantiate.rs
@@ -6,7 +6,13 @@ use cosmwasm_schema::cw_serde;
 #[cfg_attr(not(feature = "cosmwasm"), derive(Clone, Debug, PartialEq))]
 #[derive(Eq)]
 pub struct InstantiateMessage {
+	#[schemars(with = "String")]
+	#[serde(with = "Base64", default)]
 	pub client_state: Bytes,
+	#[schemars(with = "String")]
+	#[serde(with = "Base64", default)]
 	pub consensus_state: Bytes,
+	#[schemars(with = "String")]
+	#[serde(with = "Base64", default)]
 	pub checksum: Bytes
 }
