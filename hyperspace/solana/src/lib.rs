58f92a87f38b502d4105d373b911070215fcf9cd
dhruvja <dhruv@iamsizzling.com>
2024-06-27 09:20:26 -0400

    change block time to 1 minute

--- hyperspace/solana/src/lib.rs
+++ hyperspace/solana/src/lib.rs
@@ -1367,7 +1367,7 @@ deserialize client state"
 
 	fn expected_block_time(&self) -> Duration {
 		// solana block time is roughly 400 milliseconds
-		Duration::from_secs(3600)
+		Duration::from_secs(60)
 	}
 
 	async fn query_client_update_time_and_height(
